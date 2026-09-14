011afdf44ab64d2210ed1653c8b04a0b2b9a63f1
Basmallah Hossam <basmallahhossam36@gmail.com>
2026-09-14 19:38:28 +0300

    Fix CORS preflight

--- auth-module/server.js
+++ auth-module/server.js
@@ -7,13 +7,26 @@ const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
-const corsOptions = {
-    origin: "https://el-ghandoura-frontend.vercel.app",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-};
+// CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
 
-app.use(cors(corsOptions));
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
+
+app.use(cors());
 
 app.use(express.json());
 
